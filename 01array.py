b4a8d563672ea6dfcbeb4bbccfcdd7e41e5700e2
shri50 <shridharshende50@gmail.com>
2024-10-16 22:16:25 +0530

    comment

--- 01array.py
+++ 01array.py
@@ -152,13 +152,13 @@ def find_leader(arr):
 
 
 def call_find_leader():
-    # arr1 = [16, 17, 4, 3, 5, 2]
-    # result1 =  find_leader(arr1)
-    # print(f"Leader from arr = {result1}")
+    arr1 = [16, 17, 4, 3, 5, 2]
+    result1 =  find_leader(arr1)
+    print(f"Leader from arr = {result1}")
     
-    # arr2 = [1, 2, 3, 4, 5, 2]
-    # result2 =  find_leader(arr2)
-    # print(f"Leader from arr = {result2}")
+    arr2 = [1, 2, 3, 4, 5, 2]
+    result2 =  find_leader(arr2)
+    print(f"Leader from arr = {result2}")
 
 
 if __name__ == "__main__":
